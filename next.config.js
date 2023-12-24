e9e45677326431d6a8a90b0b8149310a94178809
Bruce <recall4056@gmail.com>
2023-12-25 00:55:03 +0800

    chore: update

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: '',
+  //       port: '',
+  //       pathname: '/app/uploads/**'
+  //     }
+  //   ]
+  // }
+}
+
+module.exports = nextConfig
